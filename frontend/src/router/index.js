eb4e5d398bf02489d9d98d95096e19267e1b8b5d
elumiryy <yerim5988jg@naver.com>
2024-04-30 11:55:25 +0900

    Chore: Vue Router 설정
    페이지 이동을 위해서 Router 및 연결 주소 설정
    
    Resolves:
    Related to:

---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import {createRouter, createWebHistory} from "vue-router";
+import MainView from '@/views/MainView.vue'
+import SignupView from '@/views/SignupView.vue'
+
+const routes = [
+  {
+    path:"/ssafyworld",
+    name: "MainView",
+    component: MainView
+  },
+  {
+    path:"/signup",
+    name: "SignupView",
+    component: SignupView
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export {router}
