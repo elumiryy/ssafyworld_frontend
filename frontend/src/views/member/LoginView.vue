f2a22d2ac5b69ca868c73cce091e2901d7b0ac4d
ma-an-jong <alswhd1113@gmail.com>
2024-05-04 16:54:18 +0900

    feat: google 로그인 구현
    jwt 받아서 localStorage에 넣는것 까지 완성
    라우팅 부분은 추가 구현 필요
    
    Resolves:
    Related to:

--- frontend/src/views/member/LoginView.vue
+++ frontend/src/views/member/LoginView.vue
@@ -1,15 +1,90 @@
+
 <template>
     <div class="login">
         <img class="background-img" src="@/assets/loginImg/login-background-2.png" alt="login-background-img">
         <img class="logo-img" src="@/assets/loginImg/ssafyworld-logo.png" alt="ssafyworld-logo-img">
-        <button class="google-login-btn">구글 로그인</button>
+        <button class="google-login-btn" @click="login">구글 로그인</button>
     </div>
 </template>
 
 <script>
+import { googleSdkLoaded } from "vue3-google-login";
+import axios from "axios";
+
 export default {
-     
-}
+ data() {
+    return {
+      userDetails: null
+    };
+  },
+  name: "YourComponent",
+  methods: {
+    login() {
+      googleSdkLoaded(google => {
+        google.accounts.oauth2
+          .initCodeClient({
+            client_id:process.env.VUE_APP_CLIENT_ID,
+            scope: "email profile",
+            redirect_uri: "http://localhost:8080/auth/callback",
+            callback: response => {
+              if (response.code) {
+                this.sendCodeToBackend(response.code);
+              }
+            }
+          })
+          .requestCode();
+      });
+    },
+    async sendCodeToBackend(code) {
+      try {
+        const response = await axios.post(
+          "https://oauth2.googleapis.com/token",
+          {
+            code,
+            client_id: process.env.VUE_APP_CLIENT_ID,
+            client_secret: process.env.VUE_APP_CLIENT_SECRET,
+            redirect_uri: "postmessage",
+            grant_type: "authorization_code"
+          }
+        );
+
+        const accessToken = response.data.access_token;
+
+        // Fetch user details using the access token
+        const userResponse = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+          }
+        );
+
+        const sub = userResponse.data.sub
+        const provider = 'google'
+
+        const serverResponse = await axios.post(
+            "/member/login",
+            {
+                sub,
+                provider
+            }
+        );
+
+        if(serverResponse.code === 404) {
+            //route code
+            console.log(serverResponse.data);
+        } else {
+            const jwt = serverResponse.data;
+            localStorage.setItem("accessToken", jwt);
+        }
+
+      } catch (error) {
+        console.error("Token exchange failed:", error.response.data);
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
