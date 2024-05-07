d0a715787d0e0a9023fabda0442ccef79bfa614c
elumiryy <yerim5988jg@naver.com>
2024-05-07 21:36:09 +0900

    Refactor: 시작 메뉴 on/off 동작 코드 변경
    시작메뉴가 켜지면 가로 100%를 차지하는 오류를 해결하기 위해 시작 메뉴 on/off 코드 먼저 변경함
    
    Resolves:
    Related to:

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,21 +5,39 @@
     <!-- router 설정 -->
     <router-view></router-view>
 
-    <!-- Footer -->
-    <div class="setting-footer">
-      <FooterView />
+    <!-- menu -->
+    <div class="menu">
+      <SettingView v-if="isSettingVisible" />
     </div>
+
+    <!-- Footer -->
+    <footer>
+      <FooterView @open-start-modal="callOpenStartModal" />
+    </footer>
 
   </div>
 </template>
 
 <script>
 import FooterView from '@/components/common/FooterView.vue'
+import SettingView from '@/components/common/SettingView.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isSettingVisible: false
+    };
+  },
   components: {
-    FooterView
+    FooterView,
+    SettingView
+  },
+  methods: {
+    callOpenStartModal() {
+      this.isSettingVisible = !this.isSettingVisible
+      console.log("상태 ", this.isSettingVisible)
+    }
   }
 }
 </script>
@@ -30,7 +48,12 @@ export default {
     overflow: hidden;
   }
 
-  .setting-footer {
+  .menu {
+    position: absolute;
+    bottom: 40px; /* FooterView : #footer css의 height: 40px;랑 같게 */
+  }
+
+  footer {
     width: 100%;
     /* 하단에 고정 */
     position: absolute;
