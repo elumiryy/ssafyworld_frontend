957c37f053e7b05223304b550a236d8448513b72
elumiryy <yerim5988jg@naver.com>
2024-05-07 20:15:48 +0900

    Design: 바탕화면 아이콘 범위 수정
    바탕화면 아이콘 클릭 범위가 가로 100%임을 해결하기 위해 CSS를 수정함
    
    Resolves:
    Related to:

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,18 +1,17 @@
 <template lang="">
     <div class="main">
-        <router-link to="/login">
-            <div class="windows-icon-div">
+        <div class="windows-icon-div">
+            <router-link to="/login">
                 <img src="@/assets/windowsIcon/computer_explorer-4.png" alt="windows-icon-img" width="50" height="50">
                 <p>내 컴퓨터</p>
-            </div>
-        </router-link>
-
-        <router-link to="/chat">
-            <div class="windows-icon-div">
+            </router-link>
+        </div>
+        <div class="windows-icon-div">
+            <router-link to="/chat">
                 <img src="@/assets/windowsIcon/windows.png" alt="windows-icon-img" width="50" height="50">
-                <p>채팅방 이동</p>
-            </div>
-        </router-link>
+                <p>채팅방</p>
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -21,32 +20,40 @@
 
 </script>
 
-<style>
+<style scoped>
 
     .main {
         height: 100vh;
         background-color: #008080;
     }
 
-    .main > a {
-        outline: none;
-        text-decoration: none;
-    }
-
     .windows-icon-div {
         width: 60px;
-        padding: 5px;
-        background-color: transparent;
+        padding: 15px;
+    }
+
+    .windows-icon-div > a {
+        text-decoration: none;
         display: flex;
         flex-direction: column;
         align-items: center;
     }
 
-    .windows-icon-div > p {
+     a {
+        /* width: 60px; */
+        /* padding: 5px; */
+        /* background-color: transparent; */
+        /* display: flex; */
+        /* flex-direction: column; */
+        /* align-items: center; */
+    }
+
+    .windows-icon-div p {
         margin: 0;
         padding: 1px 0;
         color: #F8FCF8;
         font-size: small;
+        /* text-align: center; */
     }
 
 </style>
