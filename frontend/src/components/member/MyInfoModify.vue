25468f68293858886c8aab644e8ec0e029c820f9
ma-an-jong <alswhd1113@gmail.com>
2024-05-07 22:55:00 +0900

    feat : 마이 페이지 수정 기능 구현
    마이 페이지 수정 API 요청 및 질문 리스트 조회 기능 구현

--- frontend/src/components/member/MyInfoModify.vue
+++ frontend/src/components/member/MyInfoModify.vue
@@ -5,28 +5,73 @@
             <p>내 정보 수정은 이름만 변경할 수 있습니다. 변경하고자 하는 사용자 이름과 본인 확인용 질문 그리고 답을 입력해 주십시오.</p>
         </div>
         <div class="field-row">
+
             <div class="mymodify-div">
                 <label for="name">이름:</label>
-                <input id="name" type="text"/>
+                <input id="name" type="text" v-model="name"/>
             </div>
+
             <div class="mymodify-div">
                 <label for="question">질문:</label>
-                <select id="question" name="question">
-                    <option value="1">아버지 성함은?</option>
-                    <option value="2">어렸을 적 살던 동네는?</option>
-                    <option value="3">졸업한 초등학교 이름은?</option>
-                    <option value="4">가장 친한 친구 이름은?</option>
+                <select id="question" name="question" v-model="questionId">
+                    <option :value="question.id" v-for="question in questions" :key=question.id>{{question.question}}</option>
                 </select>
             </div>
+
             <div class="mymodify-div">
-                <label for="answer">답:</label>
-                <input class="answer-input" id="answer" type="text"/>
+                <label for="answer">답: </label>
+                <input class="answer-input" id="answer" type="text" v-model="answer"/>
             </div>
         </div>
+        <button @click="update">수정</button>
     </div>
 </template>
 
 <script setup>
+import {ref} from 'vue';
+import axios from 'axios';
+
+const accessToken = localStorage.getItem('accessToken');
+const answer = ref("");
+const name = ref("");
+const questionId = ref(0);
+
+const questions = ref([]);
+
+async function getQuestions() {
+    await axios.get(
+            '/question',
+            {
+                headers : {
+                    Authorization : `Bearer ${accessToken}`
+                }
+            }
+        ).then(response => {
+            questions.value = response.data;
+        });
+}
+
+getQuestions();
+
+async function update() {
+   
+    console.log(answer.value, name.value, questionId.value, accessToken)
+
+    await axios.put(
+        '/member'
+        ,{
+            name : name.value,
+            questionId : questionId.value,
+            answer : answer.value
+        },{
+            headers : {
+                Authorization : `Bearer ${accessToken}`
+            }
+        }
+    );
+
+    window.close();
+}
 
 </script>
 
