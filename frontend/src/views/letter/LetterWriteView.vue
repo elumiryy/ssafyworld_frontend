ba2dcfdd3cf191e7c8719f49a8d9826751b058f1
elumiryy <yerim5988jg@naver.com>
2024-05-10 01:59:20 +0900

    Design: 편지 쓰기 페이지 디자인
    받는 사람, 제목, 내용, 버튼(다시 작성, 취소, 보내기)를 포함하여 페이지 디자인
    
    Resolves:
    Related to:

--- frontend/src/views/letter/LetterWriteView.vue
+++ frontend/src/views/letter/LetterWriteView.vue
@@ -1,20 +1,44 @@
-
-  
 <template>
   <div class="letterWrite">
+    <!--
       <div style="display: inline">
-          <h3>편지쓰기</h3>
-      </div>
-      <div style="display: inline">
-          <p>{{ordinal}} + "기 " + {{region }} + " " + {{ban}} + "반 " + {{names}} + "에게.."</p>
+        
+      </div> -->
 
-          <textarea style="width: 300px; height: 100px;"/>
-      </div>
-      <div style="display: inline">
-        <button @click="send">보내기</button>
+      <div class="window">
+        <div class="title-bar">
+          <div class="title-bar-text">
+            <img src="@/assets/windowsIcon/desktop-1.png" alt="windows-icon-img" width="10" height="10">
+            편지 작성 관리자
+          </div>
+          <div class="title-bar-controls">
+            <button aria-label="Minimize"></button>
+            <button aria-label="Maximize"></button>
+            <button aria-label="Close"></button>
+          </div>
+        </div>
+
+        <div class="window-body">
+          <img src="@/assets/windows-menu-bar.png" alt="windows-icon-img" width="100%">
+          <div class="letter-content-div">
+            <div>
+              <label for="to">To:</label>
+              <input id="to" type="text" :value="ordinal + '기 ' + region + ' ' + ban + '반 ' + names + '에게..'" disabled />
+            </div>
+            <div>
+              <label for="title">Title:</label>
+              <input id="title" type="text" placeholder="input title.." />
+            </div>
+            <textarea name="content" id="content" placeholder="input text.."></textarea>
+          </div>
+          <div class="letter-write-btn">
+            <button @click="reset">다시 작성</button>
+            <button @click="cancel">취소</button>
+            <button @click="send">보내기</button>
+          </div>
+        </div>
       </div>
   </div>
-
 </template>
 
 <script>
@@ -49,7 +73,15 @@ export default{
         alert("메시지 전송 실패 ㅜㅠ!");
       });
     };
+
+    const reset = () => {
+      document.querySelector("#title").value = null
+      document.querySelector("#content").value = null
+    }
     
+    const cancel = () => {
+      window.close()
+    }
 
     // 반환할 값들을 객체로 묶어서 반환
     return {
@@ -57,7 +89,9 @@ export default{
       region,
       ban,
       names,
-      send
+      send,
+      cancel,
+      reset
     };
   }
 }
@@ -65,6 +99,63 @@ export default{
 
 
 <style scoped>
+  .letterWrite {
+    height: 100vh;
+    position: relative;
+    z-index: 1;
+  }
+
+  .window {
+    height: 100%;
+  }
+
+  .window-body {
+    height: 100%;
+    margin: 0;
+  }
+
+  .letter-content-div {
+    height: 65%;
+    padding: 5px 10px;
+  }
+
+  .letter-content-div > div {
+    padding-bottom: 3px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .letter-content-div label, .letter-content-div input {
+    height: 25px;
+    font-size: small;
+  }
+
+  .letter-content-div input {
+    width: 90%;
+    background-color: white;
+    color: black;
+  }
+
+  .letter-content-div textarea {
+    width: 100%;
+    height: 75%;
+    padding: 5px 10px;
+    border: 3px ridge #d1d1d1;
+    border-top: 6px ridge #d1d1d1;
+    border-radius: 5px;
+    resize: none;
+    font-size: small;
+  }
+
+  .letter-write-btn {
+    padding: 0 10px;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .letter-write-btn > button {
+    margin-left: 5px;
+  }
 
 </style>
 
