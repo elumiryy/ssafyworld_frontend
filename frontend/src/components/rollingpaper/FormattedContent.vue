128ac32ab78c94ee4d74665023b918f97e8992a3
yechanissm <106381504+yechanissm@users.noreply.github.com>
2024-05-15 19:45:01 +0900

    쪽지함 띄워쓰기 가능

---
+++ frontend/src/components/rollingpaper/FormattedContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="output-content" v-html="formattedContent"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    formattedContent() {
+      return this.content ? this.content.replace(/\n/g, '<br>') : '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+.output-content {
+  white-space: pre-wrap; /* Enable line breaks */
+  overflow-y: auto; /* Enable vertical scrolling */
+}
+</style>
--- frontend/src/components/rollingpaper/ReceivedListComponent.vue
+++ frontend/src/components/rollingpaper/ReceivedListComponent.vue
@@ -1,64 +1,67 @@
 <template>
   <div class="receivedlist">
-  <div class="sunken-panel letter-list">
-    <table style="width: 100%">
-      <thead>
-        <tr>
-          <th style="width: 10%">보낸 사람</th>
-          <th style="width: 35%">제목</th>
-          <th style="width: 35%">내용</th>
-          <th style="width: 20%">전송 날짜</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="letter in letters"
-          :key="letter.letterId"
-          @click="showLetterDetail(letter)"
-          tabindex="0"
-          :class="{ 'selected-row': selectedLetter === letter }"
-        >
-          <td>{{ letter.fromUser }}</td>
-          <td>{{ truncate(letter.title) }}</td>
-          <td>{{ truncate(letter.content) }}</td>
-          <td>{{ letter.createdAt }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="sunken-panel letter-list">
+      <table style="width: 100%">
+        <thead>
+          <tr>
+            <th style="width: 10%">보낸 사람</th>
+            <th style="width: 35%">제목</th>
+            <th style="width: 35%">내용</th>
+            <th style="width: 20%">전송 날짜</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="letter in letters"
+            :key="letter.letterId"
+            @click="showLetterDetail(letter)"
+            tabindex="0"
+            :class="{ 'selected-row': selectedLetter === letter }"
+          >
+            <td>{{ letter.fromUser }}</td>
+            <td>{{ truncate(letter.title) }}</td>
+            <td>{{ truncate(letter.content) }}</td>
+            <td>{{ letter.createdAt }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
-  <div class="letter-detail">
-    <div class="letter-info">
-      <div class="letter-info-div-first">
-        <span><b>보낸 사람</b> &nbsp;:&nbsp; {{ selectedLetter.fromUser }} </span>
-        <button v-if="selectedLetter.fromUser != null" @click="hideLetter">숨기기</button>
+    <div class="letter-detail">
+      <div class="letter-info" v-if="selectedLetter.fromUser">
+        <div class="letter-info-div-first">
+          <span><b>보낸 사람</b> &nbsp;:&nbsp; {{ selectedLetter.fromUser }} </span>
+          <button v-if="selectedLetter.fromUser != null" @click="hideLetter">숨기기</button>
+        </div>
+        <div class="letter-info-div-second">
+          <span class="letter-info-div-title"><b>제목</b> &nbsp;:&nbsp; {{ selectedLetter.title }}</span>
+          <span class="letter-info-div-createdAt"><b>전송 날짜</b> &nbsp;:&nbsp; {{ selectedLetter.createdAt }}</span>
+        </div>
       </div>
-      <div class="letter-info-div-second">
-        <span class="letter-info-div-title"><b>제목</b> &nbsp;:&nbsp; {{ selectedLetter.title }}</span>
-        <span class="letter-info-div-createdAt"><b>전송 날짜</b> &nbsp;:&nbsp; {{ selectedLetter.createdAt }}</span>
+      <div class="sunken-panel letter-content">
+        <FormattedContent :content="selectedLetter.content" />
       </div>
     </div>
-    <div class="sunken-panel letter-content">{{ selectedLetter.content }}</div>
   </div>
-</div>
 </template>
 
 <script setup>
 import { defineProps, defineEmits, ref } from "vue";
-import {truncate} from '@/components/letter.js'
-import axios from 'axios'
+import { truncate } from '@/components/letter.js';
+import axios from 'axios';
+import FormattedContent from '@/components/rollingpaper/FormattedContent.vue'; // 새로운 컴포넌트 임포트
 
 defineProps({
   letters: Array,
 });
-const emit = defineEmits(['hideLetter'])
+const emit = defineEmits(['hideLetter']);
 
 const selectedLetter = ref({});
 
 const showLetterDetail = (letter) => {
   selectedLetter.value = letter;
   selectedLetter.value.createdAt = selectedLetter.value.createdAt.replace("T", " ");
-};  
+};
 
 const hideLetter = () => {
   axios.post("/letter/hidden", {
@@ -66,16 +69,15 @@ const hideLetter = () => {
     hidden: true,
   }, {
     headers: {
-          Authorization: window.localStorage.getItem("accessToken"),
-      }
-  }
-  ).then(() => {
+      Authorization: window.localStorage.getItem("accessToken"),
+    }
+  }).then(() => {
     selectedLetter.value = {};
-    emit('hideLetter')
+    emit('hideLetter');
   }).catch(error => {
     console.error("Failed to hide letter:", error);
   });
-}
+};
 </script>
 
 <style scoped>
@@ -95,11 +97,9 @@ table {
   font-size: 14px;
 }
 
-.letter-detail {  
-  /* margin-top: 5px; */
+.letter-detail {
   height: 50%;
   background-color: white;
-  /* border-style: inset; */
   box-shadow: inset -1px -1px #626262, inset 1px 1px #fff, inset -2px -2px grey, inset 2px 2px #dfdfdf;
 }
 
@@ -108,7 +108,6 @@ table {
   padding: 3px;
   font-size: 14px;
   background-color: #bfbfbf;
-  /* box-shadow: 1px 1px 1px 1px #8f8f8f; */
   box-shadow: inset -1px -1px #626262, inset 1px 1px #fff, inset -2px -2px grey, inset 2px 2px #dfdfdf;
   display: flex;
   flex-direction: column;
@@ -143,6 +142,7 @@ table {
   height: 70%;
   padding: 0 10px;
   font-size: 15px;
+  overflow-y: auto; /* Enable vertical scrolling */
 }
 
 tr > td {
