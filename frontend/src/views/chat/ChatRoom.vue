22fac6718111307346766e00eb58898115da6cc0
Juhyorim <joohr1234@gmail.com>
2024-05-15 22:35:13 +0900

    fix: 채팅 오류 수정 - 예림

--- frontend/src/views/chat/ChatRoom.vue
+++ frontend/src/views/chat/ChatRoom.vue
@@ -3,7 +3,6 @@
     <div class="window">
         <div class="title-bar">
           <div class="title-bar-text">
-            <img src="@/assets/windowsIcon/desktop-1.png" alt="windows-icon-img" width="10" height="10">
             채팅방 관리자
           </div>
           <div class="title-bar-controls">
@@ -14,69 +13,55 @@
         </div>
 
         <div class="window-body">
-            <!-- <ul>
-                <li v-for="chat in chatData" :key='chat'> {{chat}}</li>
-            </ul> -->
-
-            <!-- 테스트용 코드 -->
             <div class="sunken-panel">
                 <table class="interactive">
                     <thead>
                         <tr>
-                            <th>이름</th>
-                            <th>날짜</th>
-                            <th>내용</th>
+                          <th class="th-name">이름</th>
+                          <th class="th-time">날짜</th>
+                          <th class="th-content">내용</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr v-for="chat in chatData" :key='chat.time'>
-                            <td class="chat-name-td">{{chat.sender}}</td>
-                            <td class="chat-time-td">{{chat.time}}</td>
-                            <td class="chat-content-td">{{chat.content}}</td>
+                          <td class="chat-name-td"><span>{{chat.sender}}</span></td>
+                            <td class="chat-time-td"><span>{{chat.time}}</span></td>
+                            <td class="chat-content-td"><span>{{chat.content}}</span></td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-
             <div class="mycontent-div">
                 <label for="mycontent">채팅 입력 &nbsp; : &nbsp; </label>
                 <input id="mycontent" type="text" @keypress.enter = "sendChat" v-model="inputText" />
             </div>
-            <div style="height: 40px"></div> <!-- footer에 가려지지 않도록 처리 -->
         </div>
     </div>
   </div>
 </template>
-
 <script setup>    
 import axios from 'axios';
 import {ref, onBeforeMount, onUpdated} from 'vue';
 import {onBeforeRouteLeave, useRouter} from 'vue-router'
 import moment from 'moment';
 import {adjustTime} from '@/components/timezone.js'
-
 const router = useRouter();
-
 function closwMypage() {
    router.push({ name: 'MainView' });
 }
-
 function Chat(sender, content,time) {
     this.sender = sender;
     this.content = content;
     this.time = adjustTime(time);
 }
-
 const searchParams = new URLSearchParams(window.location.search);
 const chatRoomId = searchParams.get('chatRoomId')
 const accessToken = localStorage.getItem('accessToken');
 const chatData = ref([]);
-
 async function setChatData(response) {
     chatData.value = response.data
     .map(data => new Chat(data.senderName, data.content,data.createdAt));
 }
-
 async function getChattingData() {
     return await axios.get(
         `/chat/${chatRoomId}/messages`,
@@ -87,14 +72,11 @@ async function getChattingData() {
         }
     ).then(response => setChatData(response));
 }
-
 var clientWebSocket = undefined;
-
 onUpdated(() => {
   const bottom = document.querySelector('.sunken-panel');
   bottom.scrollTop =  bottom.scrollHeight;
 })
-
 onBeforeMount(() => {
   getChattingData();
   clientWebSocket = new WebSocket(process.env.VUE_APP_WEB_SOCKET_URL + chatRoomId);
@@ -102,19 +84,15 @@ onBeforeMount(() => {
       const chat = JSON.parse(message.data);
       chat.time = adjustTime(chat.time);
       chatData.value.push(chat);
-
       const bottom = document.querySelector('.sunken-panel');
       bottom.scrollTop =  bottom.scrollHeight;
   }
 })
-
 onBeforeRouteLeave(async (to,from) => {
   clientWebSocket.close();
   to,from;
 })
-
 const inputText = ref('');
-
 const sendChat = () => {
     inputText.value = inputText.value.trim();
   
@@ -122,12 +100,10 @@ const sendChat = () => {
       window.alert("글자 수가 너무 적습니다." , inputText.value);
       return;
     }
-
     if(inputText.value.length > 200) {
       window.alert("글자 수 제한을 초과하였습니다.");
       return;
     }
-
     clientWebSocket.send(JSON.stringify({
         "groupInfoId" : chatRoomId,
         "accessToken" : accessToken,
@@ -136,57 +112,54 @@ const sendChat = () => {
     }));
     inputText.value = '';
 }
-
 </script>
-
 <style scoped>
   .chatroom {
     height: 100vh;
   }
-
   .window {
     height: 100%;
   }
-
   .window-body {
-    height: 100%;
+    height: 90%;
     margin: 0;
     background-color: #314CA5;
   }
-
   .sunken-panel {
     width: 100%;
-    bottom: 80px;
-    height: 85%;
+    height: 90%;
     background-color: #314CA5;
-    position: fixed;
   }
   /* 스크롤바 숨기기(마우스 휠로 스크롤 가능) */
   .sunken-panel::-webkit-scrollbar {
     display: none;
   }
-
   table {
     width: 100%;
     background-color: #314CA5;
+    table-layout: fixed;
   }
-
   thead {
     width: 100%;
   }
-
   th {
     background-color: #314CA5;
     color: #E6F5FF;
     font-size: large;
     box-shadow: none;
   }
-
+  .th-name {
+    width: 10%;
+    text-align: center;
+  }
+  .th-time {
+    width: 20%;
+    text-align: center
+  }
   td {
     color: #E6F5FF;
     font-size: large;
   }
-
   table {
     border-collapse: separate;
     border-spacing: 5px;
@@ -194,37 +167,37 @@ const sendChat = () => {
   
   .chat-name-td {
     width: 15%;
+    text-align: center
   }
-
   .chat-time-td {
     width: 15%;
+    text-align: center
   }
-
   .chat-content-td {
     width: 70%;
     white-space: normal; /* 자동 줄바꿈 */
   }
-
+  .chat-name-td > span, .chat-time-td > span {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+  }
+  .chat-content-td > span {
+    height: 100%;
+    display: flex;
+    align-items: flex-start;
+  }
   .mycontent-div {
     padding: 10px;
     display: flex;
-    position: fixed;
-    bottom: 40px;
-    /* width: 100%; */
-    left: 0;
-    right: 0;
   }
-
   label {
-    width: fit-content; /* 내용에 맞게 너비 조절 */
-    min-width: 0; /* 최소 너비 */
     color: #E6F5FF;
     font-size: large;
   }
-
   input {
-    width: 80%;
+    width: 95%;
     font-size: large;
-    flex: 1;
   }
 </style>
